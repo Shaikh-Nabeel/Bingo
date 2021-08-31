a962621bf9c997ef9bbf05e8e6a79ac76313a6e6
Shaikh-Nabeel <sleeban162@gmail.com>
2021-08-31 15:37:24 +0530

    updated delete button

---
+++ app/src/main/java/com/nabeel130/bingo/Helper.java
@@ -0,0 +1,9 @@
+package com.nabeel130.bingo;
+
+import android.graphics.Bitmap;
+
+public class Helper {
+    public static String songName;
+    public static String songDuration;
+    public static Bitmap bitmap;
+}
